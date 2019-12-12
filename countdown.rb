919526483d346d2b5362c097397f5a792c3487c1
Martin Chaplin <martin.chaplin44@gmail.com>
2019-12-12 22:35:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,6 @@
 #write your code here
 
+<<<<<<< HEAD
 def countdown(num) 
   while num > 0 
      puts "#{num} SECOND(S)!"
@@ -7,3 +8,14 @@ def countdown(num)
    end  
   return "HAPPY NEW YEAR!"
 end 
+=======
+def countdown
+  number = 10
+  while number <= 10
+       puts "#{number} SECONDS(S)!"
+       number -= 1
+       return "Happy NEW YEAR!"
+  
+end
+end
+>>>>>>> a665a13776344a37796fe64e9bd58fa7fbf6f8e8
